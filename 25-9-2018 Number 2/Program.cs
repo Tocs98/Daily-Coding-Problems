72d346c5f2923bb20e669ee24273276c1a443abf
Tocs98 <toxedits@gmail.com>
2018-09-25 22:47:43 +0200

    25-9-2018 Part 2

---
+++ 25-9-2018 Number 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int[] calculateProduct(int[] numbers)
+        {
+            int[] finalArray = new int[numbers.Length];
+            int result = 1;
+
+            for(int i=0; i<numbers.Length; i++)
+            {
+                for(int x = 0; x < numbers.Length; x++)
+                {
+                    if (i != x)
+                    {
+                        result = result * numbers[x];
+                    }
+                }
+                finalArray[i] = result;
+                result = 1;
+            }
+            return finalArray;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbersToCalculate = new int[] { 3, 2, 1 };
+            Console.Write(string.Join(" ",calculateProduct(numbersToCalculate)));
+            Console.ReadLine();
+        }
+    }
+}
