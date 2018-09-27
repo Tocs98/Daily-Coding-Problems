ca93a7257d22e37329140d68a8c1fb9efb6275e8
Tocs98 <toxedits@gmail.com>
2018-09-27 21:26:29 +0200

    27-9-2018
    New daily problem, and readme extensions changed: .me -> .txt

---
+++ 27-9-2018/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int firstMissingPositive(int[] numbers)
+        {
+            int lowerNumber = 0;
+
+            for(int i = 0; i < numbers.Length; i++) //Looking for the lowest number posible in the array
+            {
+                if(numbers[i] >= 0 && lowerNumber > numbers[i]) //Positive number
+                {
+                    lowerNumber = numbers[i]; 
+                }
+            }
+
+            for(int x = 0; x < numbers.Length; x++)
+            {
+                if(numbers[x] >= 0 && lowerNumber +1 == numbers[x]) // Lowest positive integer that does not exist in the array
+                {
+                    lowerNumber = numbers[x];
+                    x = 0;
+                }
+            }
+            return lowerNumber+1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbersToCalculate = new int[] { 1, 2, 0, -5, 10 };
+            Console.Write(firstMissingPositive(numbersToCalculate));
+            Console.ReadLine();
+        }
+    }
+}
